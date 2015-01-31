7b95228e011610efd45d94ee5bfc4ded977ce4e8
ak792 <ark73@georgetown.edu>
2015-01-30 20:46:17 -0500

    redid oauth2.php to be a script again. now, header works...

--- oauth2.php
+++ oauth2.php
@@ -1,20 +1,44 @@
 <?php
-// require_once 'google-api-php-client/autoload.php';
-// require_once 'credentials.php';
-require_once 'Authorizer.php';
+require_once 'google-api-php-client/autoload.php';
+require_once "credentials.php";
+require_once "SL_DBHandler.php";
 
-if (isset($_GET['code'])){
-	$authCode = $_GET['code'];
-}
-else if (isset($_GET['error'])){
-	echo "error: " . $_GET['error'];
-	die();
-}
+	if (isset($_GET['code'])){
+		$authCode = $_GET['code'];
+	}
+	else if (isset($_GET['error'])){
+		echo "error: " . $_GET['error'];
+		die();
+	}
+//redirect to oauth2callback.php with no params if not part of flow (check sent data?)
 
-$authorizer = new Authorizer();
-$authorizer->authorize($authCode);
+	$client = new Google_Client();
+	$client->setClientId($clientId);
+	$client->setClientSecret($clientSecret);
+	$client->setRedirectUri($localhostRedirectUri);
+	$client->setApprovalPrompt("force");
+	$client->setAccessType("offline");
+	$client->setLoginHint();
+	$client->addScope("https://www.googleapis.com/auth/calendar");
 
-echo "done";
-//header("Location: localhost");
-die()
+	$authUrl = $client->createAuthUrl();
+
+	if (!isset($authCode)){
+		header('Location: ' . $authUrl);
+		die();
+	}
+
+	$accessTokenJson = $client->authenticate($authCode);
+	$accessToken = json_decode($accessTokenJson);
+
+	require_once "header.php";
+
+	if ($accessToken){
+		echo "<br>retrieved access token<br>";
+	}
+	
+	$dbHandler = new SL_DBHandler();
+	$dbHandler->setToken($accessToken);
+ 
+	require_once "footer.php";
 ?>
