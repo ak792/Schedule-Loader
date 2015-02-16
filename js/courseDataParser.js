29bfa7a4f28f06093c3ec48b4e2b754a505750db
ak792 <ark73@georgetown.edu>
2015-02-15 21:15:25 -0500

    making bookmarklet

--- js/courseDataParser.js
+++ js/courseDataParser.js
@@ -1,18 +1,36 @@
 var courseDataParser = {}
 
-//shorten
-courseDataParser.createCalEvent = function(course, row){
 
+courseDataParser.createCalEvent = function(course, row){
 	var calEvent = {};
 
 	calEvent.summary = course.courseName;
 	calEvent.description = 
 		"Professor: " + course.professor +
 		"\nType: " + course.classType[row];
-
 	calEvent.location = course.where[row];
 
+	var dates = this.getDateRangeAsDates(course, row);
+	var days = course.days[row].split("");
+
+	//get first day of class
+	var startEndFirstClass = this.getStartEndFirstClass(dates[0], days, course.time[row]);
+	calEvent.start = { 
+		dateTime : startEndFirstClass.start.toISOString(),
+		timeZone : "America/New_York"
+	};
+	calEvent.end = {
+		dateTime : startEndFirstClass.end.toISOString(),
+		timeZone : "America/New_York"
+	};
 	
+	var recurrence = this.buildRecurrence(dates[1], days);
+	calEvent.recurrence = [recurrence];
+
+	return calEvent;
+}
+
+courseDataParser.getDateRangeAsDates = function(course, row){
 	var dates = [];
 	dates = course.date_range[row].split(" - ")
 		.map(function(item){
@@ -22,25 +40,9 @@ courseDataParser.createCalEvent = function(course, row){
 	dates[1].setMinutes(59);
 	dates[1].setSeconds(59);
 
-	var days = [];
-	days = course.days[row].split("");
-
-	//get first day of class
-	var startEndFirstClass = this.getStartEndFirstClass(dates[0], days, course.time[row]);
-	calEvent.start = { 
-		"dateTime" : startEndFirstClass.start.toISOString(),
-		"timeZone" : "America/New_York"
-	};
-	calEvent.end = {
-		"dateTime" : startEndFirstClass.end.toISOString(),
-		"timeZone" : "America/New_York"
-	};
-	
-	var recurrence = this.buildRecurrence(dates[1], days);
-	calEvent.recurrence = [recurrence];
-
-	return calEvent;
+	return dates;
 }
+
 
 courseDataParser.getStartEndFirstClass = function(date, days, time){
 	var firstDate = this.getFirstDate(date, days);
@@ -57,30 +59,17 @@ courseDataParser.getStartEndFirstClass = function(date, days, time){
 	endDateTime.setHours(endTime.hours);
 	endDateTime.setMinutes(endTime.mins);
 
-	var res = {
+	return {
 		"start" : startDateTime,
 		"end" : endDateTime
 	};
-
-	return res;
-}
-
-courseDataParser.toTime = function(time_string){
-	var date = new Date("2000-01-01 " + time_string);
-
-	var res = 
-	{ 
-		"hours" : date.getHours(),
-	 "mins" :	date.getMinutes()
- 	};
-
-	return res;
 }
 
 courseDataParser.getFirstDate = function(date, days){
 
 	var currDate = new Date(date.getTime());
-	var daysInNums = days.map(this.dayToNum);
+	console.log('here');
+	var daysInNums = days.map(function(item){ return courseDataParser.daysMapping[item].num; });
 	
 	while (daysInNums.indexOf(currDate.getDay()) == -1 ){
 		currDate.setDate(currDate.getDate() + 1);
@@ -88,56 +77,20 @@ courseDataParser.getFirstDate = function(date, days){
 	return currDate;
 }
 
-//make into hashmaps
-courseDataParser.dayToNum = function(day){
-	var num;
+courseDataParser.toTime = function(time_string){
+	var date = new Date("2000-01-01 " + time_string);
 
-	if (day == 'S'){
-		num = 0;
-	} else if (day == 'M'){
-		num = 1;
-	} else if (day == 'T'){
-		num = 2;
-	} else if (day == 'W'){
-		num = 3;
-	} else if (day == 'R'){
-		num = 4;
-	} else if (day == 'F'){
-		num = 5;
-	} else if (day == 'S'){
-		num = 6;
-	}
-
-	return num;
-}
-
-courseDataParser.dayToTwoLetterCode = function(day){
-	var code;
-
-	if (day == 'S'){
-		code = 'SU';
-	} else if (day == 'M'){
-		code = 'MO';
-	} else if (day == 'T'){
-		code = 'TU';
-	} else if (day == 'W'){
-		code = 'WE';
-	} else if (day == 'R'){
-		code = 'TH';
-	} else if (day == 'F'){
-		code = 'FR';
-	} else if (day == 'S'){
-		code = 'SA';
-	}
-
-	return code;
+	return { 
+		hours : date.getHours(),
+		mins :	date.getMinutes()
+ 	};
 }
 
 courseDataParser.buildRecurrence = function(endDate, days){
 	var recurrence = "RRULE:FREQ=WEEKLY;UNTIL="; 
 	var recPattern = /[^a-zA-Z0-9]+/g;
 	var str = endDate.toISOString().slice(0, -5) + endDate.toISOString().slice(-1);
-	var daysToRepeat = days.map(this.dayToTwoLetterCode);
+	var daysToRepeat = days.map(function(item){ return courseDataParser.daysMapping[item].code; });
 	var daysToRepeatStr = ";BYDAY=" +  daysToRepeat.reduce(function(prevStr, curr) { return prevStr + "," + curr; });
 
 
@@ -145,4 +98,28 @@ courseDataParser.buildRecurrence = function(endDate, days){
 
 	return output;
 }
+
+courseDataParser.daysMapping = {
+   'S' : 
+		{ num : 0,
+		code : "SU"},
+	'M' : 
+		{ num : 1,
+		code : "MO"},
+	'T' : 
+		{ num : 2,
+		code : "TU"},
+	'W' : 
+		{ num : 3,
+		code : "WE"},
+	'R' : 
+		{ num : 4,
+		code : "TH"},
+	'F' : 
+		{ num : 5,
+		code : "FR"},
+	'S' : 
+		{ num : 6,
+		code : "SA"},
+};
 
--- js/driver.js
+++ js/driver.js
@@ -1,26 +1,11 @@
 //todo: 
-//not working unless i provide a login hint?
-//clean up the code
-	//shorten functions
-	//use this when possible
 //bundle into a bookmarkelet
-
-
-function run(){
-	var btn = document.createElement("div");
-	btn.id = "authorize-button";
-	btn.textContent = "mybutton!";
-	document.body.appendChild(btn);
-
+(function run(){
 	$.getScript("https://apis.google.com/js/client.js?onload=loadClient", function(){});
-	console.log('loaded script');
-
-	
-}
+})();
 
 // only add courses after gapi loads
 function addAllCourses(){
-	console.log('called back');
 	scraper.scrape();
 	for (var i = 0; i < scraper.courses.length; i++){
 		apiAdapter.insertClassEvent(courseDataParser, scraper.courses[i]);
