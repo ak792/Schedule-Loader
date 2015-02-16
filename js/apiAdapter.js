0226fb41cf44a7c3b48059bb630871652650d7f5
ak792 <ark73@georgetown.edu>
2015-02-15 22:07:45 -0500

    fix

--- js/apiAdapter.js
+++ js/apiAdapter.js
@@ -25,7 +25,7 @@ apiAdapter.addAllCourses = function(){
 		function(resp){
 			console.log(resp);
 			console.log('here');
-			apiAdapter.calendarId = resp.id;
+			apiAdapter.calendarId = resp.result.id;
 			for (var i = 0; i < scraper.courses.length; i++){
 				apiAdapter.insertClassEvent(courseDataParser, scraper.courses[i]);
 			}
